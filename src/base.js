7d87b87ede7f9c17db02aa968a572ead892a0858
bob212 <ahtohka@gmail.com>
2018-10-09 17:32:00 +0100

    first commit

---
+++ src/base.js
@@ -0,0 +1,29 @@
+var test_movies_data = [
+{
+    name: 'King Arthur: Legend of the Sword',
+    country: 'USA',
+    release: '2017-05-11',
+    description: 'An alternative version of the King Arthur legend. As a boy, Arthur is left orphaned after his father, King Uther Pendragon, and mother are killed in a war waged against them by Vortigern, who then assumes the throne. Arthur flees and is raised in a brothel, knowing very little of his birthright. Vortigern wants Arthur dead, to ensure there is no claimant to the throne. The legends foretell that only the next king will be able to draw Excalibur, Uther\'s sword, from the rock where it is lodged. So, in an effort to identify Arthur, Vortigern forces all the young men of Arthur\'s age to attempt to draw out the sword. Now it is Arthur\'s turn',
+    budget: '175000000',
+    price: '4',
+    genres: ['action', 'fantasy', 'adventure']
+},
+{
+    name: 'We\'re the Millers',
+    country: 'USA',
+    release: '2013-08-03',
+    description: 'After being robbed of a week\'s take, small-time pot dealer David is forced by his boss to go to Mexico to pick up a load of marijuana. In order to improve his odds of making it past the border, David asks the broke stripper Rose and two local teenagers to join him and pretend they\'re on a family holiday.',
+    budget: '37000000',
+    price: '2',
+    genres: ['comedy', 'crime']
+}, 
+{
+    name: 'Yes Man',
+    country: 'Great Britain',
+    release: '2008-12-09',
+    description: 'Carl Allen is at a standstill. No future... Until the day he enrolls into a personal development program based on a very simple idea: say yes to everything! Carl discovers with amazement the magical power of "Yes", and sees his professional and romantic life turned upside down overnight: an unexpected promotion and a new girlfriend. But he\'ll soon discover that better can be good\'s enemy, and that all opportunities shouldn\'t be taken.',
+    budget: '70000000',
+    price: '9',
+    genres: ['comedy']
+}, 
+];
